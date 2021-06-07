802833660f912b77c93415005f5f340516dd42cf
joony <joony@kakaopage.com>
2021-06-07 22:20:09 +0900

    🔥 toolbar_detached 제거

--- src/main/resources/db/migration/V11__create_member_config.sql
+++ src/main/resources/db/migration/V11__create_member_config.sql
@@ -5,7 +5,6 @@ CREATE TABLE `member_config`
     `global_theme`     VARCHAR(16)           NOT NULL,
     `toolbar_theme`    VARCHAR(16)           NOT NULL,
     `menu_theme`       VARCHAR(16)           NOT NULL,
-    `toolbar_detached` BOOLEAN DEFAULT FALSE NOT NULL,
     `content_boxed`    BOOLEAN DEFAULT FALSE NOT NULL,
     `primary_color`    VARCHAR(16)           NOT NULL,
     `created_by`       VARCHAR(100)          NOT NULL,
