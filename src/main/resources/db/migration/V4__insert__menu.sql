5a8855fa7bbb8281549d0f3ebf1c86d0df55dee8
joony <joony.kim@kakaoent.com>
2022-02-01 01:03:46 +0900

    update

--- src/main/resources/db/migration/V4__insert__menu.sql
+++ src/main/resources/db/migration/V4__insert__menu.sql
@@ -12,14 +12,4 @@ VALUES (1, '개발자', 'GROUP', null, 5, 'mdi-developer-board', null, 1, NOW(),
 
        (9, '시스템 관리', 'GROUP', null, 4, 'mdi-cog', null, 1, NOW(), 1, NOW()),
        (10, '코드', 'PAGE', 9, 1, null, '/management/code', 1, NOW(), 1, NOW()),
-       (11, '메뉴', 'PAGE', 9, 2, null, '/management/menu', 1, NOW(), 1, NOW()),
-
-       (12, '게시 관리', 'GROUP', null, 1, 'mdi-clipboard-edit', null, 1, NOW(), 1, NOW()),
-       (13, '공지사항', 'PAGE', 12, 1, null, '/management/notice', 1, NOW(), 1, NOW()),
-       (14, 'FAQ', 'PAGE', 12, 2, null, '/management/faq', 1, NOW(), 1, NOW()),
-       (15, '팝업', 'PAGE', 12, 3, null, '/management/popup', 1, NOW(), 1, NOW()),
-       (16, '약관', 'PAGE', 12, 4, null, '/management/terms', 1, NOW(), 1, NOW()),
-
-       (17, '앱 관리', 'GROUP', null, 2, 'mdi-apps', null, 1, NOW(), 1, NOW()),
-       (18, '앱', 'PAGE', 17, 1, null, '/management/app', 1, NOW(), 1, NOW()),
-       (19, '앱 버전', 'PAGE', 17, 2, null, '/management/app-version', 1, NOW(), 1, NOW());
+       (11, '메뉴', 'PAGE', 9, 2, null, '/management/menu', 1, NOW(), 1, NOW());
