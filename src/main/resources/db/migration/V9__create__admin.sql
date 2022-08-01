e9b98eea7da160be7b0c30a4b24fe064a706013a
joony.kim <->
2022-08-01 10:49:14 +0900

    fixx

--- src/main/resources/db/migration/V9__create__admin.sql
+++ src/main/resources/db/migration/V9__create__admin.sql
@@ -2,7 +2,7 @@ CREATE TABLE admin
 (
     id               BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
     login_id         VARCHAR(100) NOT NULL,
-    password         CHAR(128) NULL,
+    password         CHAR(80) NULL,
     name             VARCHAR(100) NOT NULL,
     role_id          BIGINT(20) NOT NULL,
     sign_in_fail_cnt INT(1) DEFAULT 0 NOT NULL,
