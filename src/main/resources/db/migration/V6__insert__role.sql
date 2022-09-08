600a3f75a4e6c5b0885627d679e53037732bcee0
joony.kim <bestheroz@gmail.com>
2022-09-09 01:19:23 +0900

    🚀Update project

--- src/main/resources/db/migration/V6__insert__role.sql
+++ src/main/resources/db/migration/V6__insert__role.sql
@@ -1,20 +1,11 @@
 INSERT INTO `role` (id, parent_id, name, available_flag, display_order, created_by, created,
                     updated_by, updated)
-VALUES (1, null, '마스터', 1, 1, 1, NOW(), 1, NOW());
-
-INSERT INTO `role` (id, parent_id, name, available_flag, display_order, created_by, created,
-                    updated_by, updated)
-VALUES (2, null, '관리자', 1, 2, 1, NOW(), 1, NOW());
-
-INSERT INTO `role` (id, parent_id, name, available_flag, display_order, created_by, created,
-                    updated_by, updated)
-VALUES (3, 2, '쓰기만', 1, 1, 1, NOW(), 1, NOW());
-
-INSERT INTO `role` (id, parent_id, name, available_flag, display_order, created_by, created,
-                    updated_by, updated)
-VALUES (4, 2, '보기만', 1, 2, 1, NOW(), 1, NOW());
-
-INSERT INTO `role` (id, parent_id, name, available_flag, display_order, created_by, created,
-                    updated_by, updated)
-VALUES (5, 2, '엑셀만', 1, 3, 1, NOW(), 1, NOW());
+VALUES (1, null, '마스터', 1, 1, 1, NOW(), 1, NOW()),
+       (2, null, '서울 관리자', 1, 2, 1, NOW(), 1, NOW()),
+       (3, 2, '을지로 관리자', 1, 1, 1, NOW(), 1, NOW()),
+       (4, 2, '잠실 관리자', 1, 2, 1, NOW(), 1, NOW()),
+       (5, 2, '서초 관리자', 1, 3, 1, NOW(), 1, NOW()),
+       (6, null, '제주 관리자', 1, 2, 1, NOW(), 1, NOW()),
+       (7, 6, '중문 관리자', 1, 1, 1, NOW(), 1, NOW())
+;
 
